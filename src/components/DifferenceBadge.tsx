9828e2f0b76ceeb96fa197f92f182693639ba1fc
Tero Laakso <tero.laakso@reaktor.com>
2022-05-17 10:06:54 +0300

    Add DifferenceBadge

---
+++ src/components/DifferenceBadge.tsx
@@ -0,0 +1,28 @@
+import classNames from "classnames";
+
+export interface DifferenceBadgeProps {
+  difference: number | null;
+}
+
+export default function DifferenceBadge({ difference }: DifferenceBadgeProps) {
+  if (difference === null) {
+    return null;
+  }
+  const classes = classNames(
+    "rounded-full",
+    "px-1",
+    "mr-1",
+    "whitespace-nowrap",
+    {
+      "bg-green-800": difference < 5,
+      "bg-yellow-800": difference >= 5 && difference < 15,
+      "bg-red-800": difference >= 15,
+    }
+  );
+  return (
+    <small className={classes}>
+      {difference > 0 ? "+" : ""}
+      {difference}
+    </small>
+  );
+}
