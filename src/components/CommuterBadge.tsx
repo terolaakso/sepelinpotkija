a3362642a0a8774af08d2be0a833133e58942a9f
Tero Laakso <tero.laakso@reaktor.com>
2022-05-17 09:42:38 +0300

    Add CommuterBadge

---
+++ src/components/CommuterBadge.tsx
@@ -0,0 +1,14 @@
+export interface CommuterBadgeProps {
+  lineId: string | null;
+}
+
+export default function CommuterBadge({ lineId }: CommuterBadgeProps) {
+  if (!lineId) {
+    return null;
+  }
+  return (
+    <small className="bg-gray-300 text-gray-900 pl-1 pr-1 rounded-sm mr-1">
+      {lineId}
+    </small>
+  );
+}
