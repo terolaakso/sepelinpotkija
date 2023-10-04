7fb348f7477024c4f4b63a44232c6279d2e6cdd0
Tero Laakso <tero.laakso@reaktor.com>
2023-10-04 14:07:14 +0300

    Remove unused page

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,12 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Connector } from '@/features/mqtt';
 import { TrainTracking } from '@/features/trainTracking';
 
-import Home from './components/Home';
-
 function App() {
   return (
     <Connector brokerUrl="wss://rata.digitraffic.fi:443/mqtt">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<TrainTracking />} />
-          <Route path="/home" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </Connector>
--- src/components/Status.tsx
+++
@@ -1,7 +0,0 @@
-import { useMqttState } from '@/features/mqtt';
-
-export default function Status() {
-  const { connectionStatus } = useMqttState();
-
-  return <div>{`Status: ${connectionStatus}`}</div>;
-}
